64a1060d01de20bdcd71a3c4059f379490cbeecb
Fialkaksu <fialkaksu1984@gmail.com>
2020-06-12 18:43:59 +0300

    Updates

---
+++ precache-manifest.fbc3d74e9fac9b44ff4aabf89784286f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37f6671f900ec987d244d4ccb6a34a5",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "98602eab1abad159eec0",
+    "url": "/React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7fc11fdc57603951e19a",
+    "url": "/React/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "98602eab1abad159eec0",
+    "url": "/React/static/js/2.339b993c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.339b993c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc11fdc57603951e19a",
+    "url": "/React/static/js/main.e545a023.chunk.js"
+  },
+  {
+    "revision": "b9f3282adc66ee87b4c0",
+    "url": "/React/static/js/runtime-main.249206fb.js"
+  }
+]);
